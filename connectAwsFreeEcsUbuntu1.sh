040bfa6a79f27713044722225bb2d0780ccebcad
Wang Sai <wangsai@xiaomi.com>
2022-03-14 00:37:30 +0800

    add mysql scripts

---
+++ connectAwsFreeEcsUbuntu1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i "/etc/aws_free_ec2_ubuntu_1.pem" ubuntu@ec2-13-250-99-186.ap-southeast-1.compute.amazonaws.com
---
+++ loginAWSMySQL.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+mysql -h 13.250.99.186 -P3306 -u dev -p
