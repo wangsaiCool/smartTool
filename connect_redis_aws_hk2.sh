72a21807e67f211f7a4aeeb7f14f0c928c252ff4
Wang Sai <wangsai@xiaomi.com>
2022-04-02 23:22:29 +0800

    update connect_redis_aws_hk2.sh

--- connect_redis_aws_hk2.sh
+++ connect_redis_aws_hk2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
-redis-cli -h ec2-16-162-118-77.ap-east-1.compute.amazonaws.com -p 6379
+# redis-cli -h ec2-16-162-118-77.ap-east-1.compute.amazonaws.com -p 6379
+redis-cli -h 16.162.118.77 -p 6379
 #mysql -h ec2-16-163-143-232.ap-east-1.compute.amazonaws.com  -P3306 -u dev -p
